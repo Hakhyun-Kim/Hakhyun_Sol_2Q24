9564707963e8be1510e6ff0fcb8a75fb0b9146e3
Hakhyun Kim <kimhakhyun@gmail.com>
2024-05-17 15:28:42 -0400

    week 3

--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -17,27 +17,27 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your image URI: ", myUri);
+        const image = "https://arweave.net/z763T2hM8FmBkmy4lCxsDmh5hlWMV5gzXLBQuIpOmgg";
+        const metadata = {
+            name: "real rug",
+            symbol: "lol",
+            description: "actual rug",
+            image: image,
+            attributes: [
+                {trait_type: 'rect', value: 'multi-color'}
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image
+                    },
+                ]
+            },
+            creators: []
+        };
+        const myUri = await umi.uploader.uploadJson(metadata);
+        console.log("Your image URI: ", myUri);
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
